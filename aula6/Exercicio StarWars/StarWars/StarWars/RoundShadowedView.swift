da1a950c29ca6b396297c8fe0648674329b8052a
InfraCWI <infracwi@MacBook-Pro-de-InfraCWI-2.local>
2017-07-10 15:10:45 -0300

    Continuacao exercicio StarWars

--- aula6/Exercicio StarWars/StarWars/StarWars/RoundShadowedView.swift
+++ aula6/Exercicio StarWars/StarWars/StarWars/RoundShadowedView.swift
@@ -17,7 +17,7 @@ class RoundShadowedView : UIView {
         self.layer.shadowColor = UIColor.black.cgColor
         self.layer.shadowOpacity = 0.15
         self.layer.shadowOffset = CGSize.zero
-        self.layer.shadowRadius = 2
-        self.layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
+        self.layer.shadowRadius = 3.0
+        //self.layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
     }
 }
