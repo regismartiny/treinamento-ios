5e7e1a234662e4eaa90902a5eb28b0c16b3c2613
InfraCWI <infracwi@MacBook-Pro-de-InfraCWI-2.local>
2017-07-13 12:03:03 -0300

    Exercicio Animacoes

--- aula10/Exercicios/Animacoes/Animacoes/ViewController.swift
+++ aula10/Exercicios/Animacoes/Animacoes/ViewController.swift
@@ -10,16 +10,78 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var centerView: CenterView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func actionShrink20Width(_ sender: Any) {
+        
+        UIView.animate(withDuration: 2, animations: {
+            self.centerView.frame.size.width = self.centerView.frame.width - 20
+        })
+        
+    }
+    
+    @IBAction func actionShrinkHeightBy20(_ sender: Any) {
+        UIView.animate(withDuration: 2, animations: {
+            self.centerView.frame.size.height = self.centerView.frame.height - 20
+        })
+    }
+    
+    @IBAction func actionMoveRightBy50(_ sender: Any) {
+        
+        UIView.animate(withDuration: 2, animations: {
+            self.centerView.frame.origin = CGPoint(x: self.centerView.frame.origin.x + 50, y: self.centerView.frame.origin.y)
+        })
+    }
+    
+    @IBAction func actionMoveLeftBy50(_ sender: Any) {
+        UIView.animate(withDuration: 2, animations: {
+            self.centerView.frame.origin = CGPoint(x: self.centerView.frame.origin.x - 50, y: self.centerView.frame.origin.y)
+        })
+    }
+    
+    @IBAction func actionMoveUpBy50(_ sender: Any) {
+        UIView.animate(withDuration: 2, animations: {
+            self.centerView.frame.origin = CGPoint(x: self.centerView.frame.origin.x, y: self.centerView.frame.origin.y - 50)
+        })
+    }
+    
+    
+    @IBAction func actionMoveDownBy50(_ sender: Any) {
+        UIView.animate(withDuration: 2, animations: {
+            self.centerView.frame.origin = CGPoint(x: self.centerView.frame.origin.x, y: self.centerView.frame.origin.y + 50)
+        })
+    }
+    
+    @IBAction func actionFadeIn(_ sender: Any) {
+        UIView.animate(withDuration: 2, animations: {
+            self.centerView.alpha = 1
+        })
+    }
+    
+    @IBAction func actionFadeOut(_ sender: Any) {
+        UIView.animate(withDuration: 2, animations: {
+            self.centerView.alpha = 0
+        })
+    }
+    
+    @IBAction func actionMakeItRounded(_ sender: Any) {
+        UIView.animate(withDuration: 2, animations: {
+            self.centerView.layer.cornerRadius = self.centerView.frame.width / 2
+        })
     }
 
+    @IBAction func actionMakeItSquared(_ sender: Any) {
+        UIView.animate(withDuration: 2, animations: {
+            self.centerView.layer.cornerRadius = 0
+        })
+    }
 
 }
 
