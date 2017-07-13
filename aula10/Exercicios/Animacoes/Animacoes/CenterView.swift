3abc9c8a1afb718aaf842f6722b7f1fe12e396fb
InfraCWI <infracwi@MacBook-Pro-de-InfraCWI-2.local>
2017-07-13 09:50:13 -0300

    Exercicio Animacoes

---
+++ aula10/Exercicios/Animacoes/Animacoes/CenterView.swift
@@ -0,0 +1,15 @@
+//
+//  CenterView.swift
+//  Animacoes
+//
+//  Created by InfraCWI on 13/07/17.
+//  Copyright © 2017 CWI. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CenterView : UIView {
+    
+    
+}
---
+++ aula10/Exercicios/Animacoes/Animacoes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animacoes
+//
+//  Created by InfraCWI on 13/07/17.
+//  Copyright © 2017 CWI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
